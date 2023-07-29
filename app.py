88e0b52ec0d6b3e535f2f467f3010c60793161f5
lakshmankashyap <lkashyap92@gmail.com>
2023-07-29 11:59:27 +0530

    Url Variable Add

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app =  Flask(__name__)
 def hello():
     return  'hello world'
 
+@app.route('/new')
+def new():
+    return 'new page'
+
+@app.route('/demo')
+def demo():
+    return 'this is demo page'
+
+#@app.route('/profile/<name>')
+@app.route('/profile/<int:id>')
+#def profile(name):
+def profile(id):
+    #return 'This profile belongs to %s' %name
+    return 'This profile belongs to %d' %id
+def demo():
+    return 'this is demo page'
+
 if __name__ == '__main__':
     app.run()
